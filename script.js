83762480c721a0825ce8142ebf6064e90184ceea
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-02-07 12:37:55 -0500

    add continuing calculation support.
    change to arrays holding values, last answer
    carries to new calculation. add ac button
    all memory and display clear.

--- script.js
+++ script.js
@@ -20,7 +20,10 @@ function operate(num1, num2, operator){
     if (!operators.includes(operator)){
         return "Error, not an operator"
     } else {
-        return window[operator](num1, num2);
+        outputArea.textContent = window[operator](num1, num2);
+        let answer = window[operator](num1, num2);
+        
+        return answer;
     }
 }
 
@@ -28,16 +31,30 @@ function operate(num1, num2, operator){
 function updateDisplay(button){
     if(outputArea.textContent == ""){
         outputArea.textContent = button.textContent;
-        let currentValue = parseFloat(button.textContent);
-        console.log(currentValue);
+        outputArea.value = button.textContent;
+        // let currentValue = parseFloat(button.textContent);
+        // console.log(currentValue);
     } else {
         outputArea.textContent = outputArea.textContent + button.textContent;
-        let currentValue = parseFloat(button.textContent) + parseFloat(outputArea.textContent);
-        console.log(currentValue);
+        outputArea.value = outputArea.value + button.textContent;
+        
+        // let currentValue = parseFloat(button.textContent) + parseFloat(outputArea.textContent);
+        // console.log(currentValue);
     }
     
 }
 
+// clear display
+function clearDisplay(){
+    outputArea.textContent = '';
+    outputArea.value = '';
+}
+
+function postCalcValueShift(answer){
+    val1[0] = answer;
+    val2.pop();
+    opVal.pop();
+}
 // console.log(operators["add"]);
 // console.log(operate(1,2,"add"));
 
@@ -48,7 +65,41 @@ const percentButton = document.querySelector("#percent");
 const divideButton = document.querySelector("#divide");
 const sevenButton = document.querySelector("#seven");
 const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector("#operate");
 
-acButton.addEventListener("click", () => outputArea.textContent ='');
-// let currentValue;
+acButton.addEventListener("click", function memClear() {
+    val1.pop() && val2.pop() && opVal.pop();
+    clearDisplay();
+});
+
+let val1 = [];
+let val2 = [];
+let opVal = [];
 numberButtons.forEach((b) => b.addEventListener("click", () => updateDisplay(b)));
+operatorButtons.forEach((b) => b.addEventListener("click", function captureVal(){
+    opVal.push(b.id);
+    console.log(opVal);
+    if (val1 == ''){
+        val1.push(parseFloat(outputArea.value));
+        console.log(val1);
+        
+    }
+    clearDisplay();
+}))
+
+equalsButton.addEventListener("click", function calc(){
+    if (val2 == ''){
+        val2.push(parseFloat(outputArea.value));
+        console.log(val2);
+    }
+
+    if (val1.length > 0 && val2.length > 0 && opVal.length > 0){
+        operate(val1[0],val2[0],opVal[0]);
+        const answer = operate(val1[0],val2[0],opVal[0]);
+        console.log("answer: ", answer);
+        postCalcValueShift(answer);
+    } else {
+        outputArea.textContent = val1[0];
+    }
+})
